ac6e694cf754a77f92093fd37e60c3c8b5654cae
dry-stan Nex <info@dry-stan.site>
2023-09-15 20:53:33 +0330

    update

--- marzban_node_cli.py
+++ marzban_node_cli.py
@@ -1,158 +1,124 @@
+#!/usr/bin/env python3
 import click
-import os
 import subprocess
+import os
 import time
 
 @click.command()
-@click.option('--iran', is_flag=True, help='Install for Iranian server with DNS modification.')
-@click.option('--international', is_flag=True, help='Install for international server without DNS modification.')
-@click.option('--certificate-key', is_flag=True, help='Get the certificate key.')
-@click.option('--auto', is_flag=True, help='Auto mode, user selects options by number.')
-def install_marzban_node(iran, international, certificate_key, auto):
-    """Marzban-node Installation Script"""
+@click.option('--dns', is_flag=True, help='Install for Iranian server (with DNS modification)')
+@click.option('--certificate', is_flag=True, help='Get the certificate key')
+@click.option('--certificate-key', is_flag=True, help='Show the certificate key (implies --certificate)')
+@click.option('--help', is_flag=True, help='Show professional help message')
+def main(dns, certificate, certificate_key, help):
+    if help:
+        show_help()
+        return
+
+    if certificate_key:
+        get_certificate_key()
+        return
 
     click.echo("Welcome to the Marzban-node Installation Script")
     click.echo("----------------------------------------------")
     click.echo("Script created by dry-stan Nex")
     click.echo("This script will help you install Marzban-node.")
-    time.sleep(3)
+    click.echo("Please wait while the installation proceeds...")
 
-    if not iran and not international and not certificate_key:
-        if auto:
-            choice = auto_select()
-        else:
-            choice = manual_select()
-
-        if choice == 1:
-            iran = True
-        elif choice == 2:
-            international = True
-        elif choice == 3:
-            certificate_key = True
-
-    if iran:
-        modify_dns_settings()
-    install_docker_and_compose()
-    install_git()
-    install_marzban_node_internal()
-
-    if certificate_key:
-        time.sleep(5)  # Wait for 5 seconds
-        clear_terminal()
-        get_certificate_key()
-
-    click.echo("Script execution complete.")
-
-def auto_select():
-    """Automatically select an option based on user input."""
     try:
-        click.echo("Choose an installation option:")
-        click.echo("1. Install for Iranian server (with DNS modification)")
-        click.echo("2. Install for international server (without DNS modification)")
-        click.echo("3. Get the certificate key")
-        choice = click.prompt("Enter the number of your choice (1/2/3):", type=int)
-        if choice not in [1, 2, 3]:
-            raise click.ClickException("Invalid choice. Please select 1, 2, or 3.")
-        return choice
-    except click.exceptions.Abort:
-        click.echo("Aborted. Exiting...")
-        raise SystemExit(1)
+        if dns:
+            modify_dns_settings()
+        install_docker_and_compose()
+        install_git()
+        install_marzban_node()
+        if not dns or certificate:
+            get_certificate_key()  # Always run get_certificate_key if not DNS or explicitly specified
+        click.echo("Installation complete.")
+    except KeyboardInterrupt:
+        click.echo("\nInstallation aborted.")
 
-def manual_select():
-    """Manually select an option based on user input."""
-    while True:
-        click.echo("Choose an installation option:")
-        click.echo("1. Install for Iranian server (with DNS modification)")
-        click.echo("2. Install for international server (without DNS modification)")
-        click.echo("3. Get the certificate key")
-        choice = click.prompt("Enter your choice (1/2/3):", type=int)
-        if choice in [1, 2, 3]:
-            return choice
-        else:
-            click.echo("Invalid choice. Please select 1, 2, or 3.")
+def show_help():
+    # Display professional help message
+    click.echo("Marzban-node Installation Script")
+    click.echo("--------------------------------")
+    click.echo("This script assists in installing Marzban-node.")
+    click.echo("Options:")
+    click.echo("  --dns: Install for Iranian server with DNS modification.")
+    click.echo("  --certificate: Get the certificate key.")
+    click.echo("  --certificate-key: Show the certificate key (implies --certificate).")
+    click.echo("  --help: Show this professional help message.")
 
 def modify_dns_settings():
-    """Modify DNS settings for Iranian server"""
     click.echo("Modifying DNS settings for Iranian server...")
     try:
-        with open("/etc/systemd/resolved.conf", "r") as resolved_conf_file:
-            lines = resolved_conf_file.readlines()
-
-        with open("/etc/systemd/resolved.conf", "w") as resolved_conf_file:
+        with open('/etc/systemd/resolved.conf', 'r') as f:
+            lines = f.readlines()
+        with open('/etc/systemd/resolved.conf', 'w') as f:
             for line in lines:
-                if line.startswith("DNS="):
-                    resolved_conf_file.write("DNS=185.51.200.2\n")
-                elif line.startswith("FallbackDNS="):
-                    resolved_conf_file.write("FallbackDNS=178.22.122.100\n")
+                if line.startswith('DNS='):
+                    f.write('DNS=185.51.200.2\n')
+                elif line.startswith('FallbackDNS='):
+                    f.write('FallbackDNS=178.22.122.100\n')
                 else:
-                    resolved_conf_file.write(line)
-
-        subprocess.run(["sudo", "systemctl", "restart", "systemd-resolved"], check=True)
+                    f.write(line)
+        subprocess.run(['sudo', 'systemctl', 'restart', 'systemd-resolved'])
         click.echo("DNS settings modified successfully.")
+        time.sleep(5)  # Sleep for 5 seconds
     except Exception as e:
-        click.echo(f"Failed to modify DNS settings: {str(e)}")
+        click.echo(f"Error modifying DNS settings: {str(e)}")
 
 def install_docker_and_compose():
-    """Install Docker and Docker Compose"""
     click.echo("Installing Docker and Docker Compose...")
     try:
+        # Install Docker using provided commands
         subprocess.run(["curl", "-fsSL", "https://get.docker.com", "-o", "get-docker.sh"], check=True)
         subprocess.run(["sh", "get-docker.sh"], check=True)
         subprocess.run(["rm", "get-docker.sh"], check=True)
 
-        subprocess.run(
-            [
-                "curl",
-                "-L",
-                f"https://github.com/docker/compose/releases/latest/download/docker-compose-{os.uname().sysname.lower()}-{os.uname().machine.lower()}",
-                "-o",
-                "/usr/local/bin/docker-compose",
-            ],
-            check=True,
-        )
-        subprocess.run(["chmod", "+x", "/usr/local/bin/docker-compose"], check=True)
+        # Install Docker Compose
+        subprocess.run(['curl', '-L', f"https://github.com/docker/compose/releases/latest/download/docker-compose-{os.uname().sysname.lower()}-{os.uname().machine}", '-o', '/usr/local/bin/docker-compose'])
+        subprocess.run(['chmod', '+x', '/usr/local/bin/docker-compose'])
+
         click.echo("Docker and Docker Compose installed successfully.")
+        time.sleep(2)  # Sleep for 2 seconds
     except Exception as e:
-        click.echo(f"Failed to install Docker and Docker Compose: {str(e)}")
+        click.echo(f"Error installing Docker and Docker Compose: {str(e)}")
 
 def install_git():
-    """Install Git"""
-    click.echo("Installing Git...")
+    click.echo("Installing git...")
     try:
-        subprocess.run(["sudo", "apt-get", "install", "-y", "git"], check=True)
+        subprocess.run(['sudo', 'apt-get', 'install', '-y', 'git'])
+        click.echo("Git installed successfully.")
+        time.sleep(1)  # Sleep for 1 second
     except Exception as e:
-        click.echo(f"Failed to install Git: {str(e)}")
+        click.echo(f"Error installing git: {str(e)}")
 
-def install_marzban_node_internal():
-    """Install Marzban-node"""
-    click.echo("Cloning Marzban-node repository...")
+def install_marzban_node():
+    click.echo("Installing Marzban-node...")
     try:
-        subprocess.run(["git", "clone", "https://github.com/Gozargah/Marzban-node"], check=True)
+        if not os.path.exists("Marzban-node"):
+            subprocess.run(['git', 'clone', 'https://github.com/Gozargah/Marzban-node'])
         os.chdir("Marzban-node")
-        click.echo("Lifting Marzban-node container...")
-        subprocess.run(["docker-compose", "up", "-d"], check=True)
-        time.sleep(5)
+        subprocess.run(['docker-compose', 'up', '-d'])
+        click.echo("Marzban-node container is running.")
+        click.echo("Please wait a few moments for it to fully initialize.")
+        click.echo("You can check the status using 'docker-compose ps'.")
+        time.sleep(5)  # Sleep for 5 seconds
     except Exception as e:
-        click.echo(f"Failed to clone and install Marzban-node: {str(e)}")
+        click.echo(f"Error installing Marzban-node: {str(e)}")
 
 def get_certificate_key():
-    """Get the certificate key"""
     click.echo("Getting the certificate key...")
     try:
-        with open("/var/lib/marzban-node/ssl_cert.pem", "r") as cert_file:
-            click.echo(cert_file.read())
+        with open('/var/lib/marzban-node/ssl_cert.pem', 'r') as f:
+            certificate_key = f.read()
+        click.echo(certificate_key)
+        time.sleep(1)  # Sleep for 1 second
     except Exception as e:
-        click.echo(f"Failed to get the certificate key: {str(e)}")
-
-def clear_terminal():
-    """Clear the terminal"""
-    if os.name == 'posix':
-        subprocess.run(['clear'])
-    elif os.name == 'nt':
-        subprocess.run(['cls'])
+        click.echo(f"Error getting the certificate key: {str(e)}")
 
 if __name__ == "__main__":
-    install_marzban_node()
+    main()
+
 
 
 
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup
 
 setup(
-    name="marzban-node-cli",
-    version="0.1",
-    py_modules=["marzban_node_cli"],
+    name='marzban-node-cli',
+    version='0.1',
+    py_modules=['marzban_node_cli'],
     install_requires=[
-        "click",
+        'Click',
     ],
-    entry_points={
-        "console_scripts": [
-            "marzban-node=marzban_node_cli:install_marzban_node",
-        ],
-    },
+    entry_points='''
+        [console_scripts]
+        marzban-node=marzban_node_cli:main
+    ''',
 )
+
 
 
