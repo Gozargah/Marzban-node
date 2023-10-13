e6fc490b6f6159d4379fc51f5869a52f2f85a77e
dry-stan Nex <info@dry-stan.site>
2023-10-13 14:54:45 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='marzban-node',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
