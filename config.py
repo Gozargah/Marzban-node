596eda32cfbd0b95f03c1c1a1aac5dfdcdce4f80
Mohammad <immohammad20000@protonmail.com>
2024-12-02 20:06:48 +0330

    change SERVICE_PROTOCOL to rest

--- config.py
+++ config.py
@@ -8,13 +8,16 @@ SERVICE_PORT = config('SERVICE_PORT', cast=int, default=62050)
 
 XRAY_API_HOST = config("XRAY_API_HOST", default="0.0.0.0")
 XRAY_API_PORT = config('XRAY_API_PORT', cast=int, default=62051)
-XRAY_EXECUTABLE_PATH = config("XRAY_EXECUTABLE_PATH", default="/usr/local/bin/xray")
+XRAY_EXECUTABLE_PATH = config(
+    "XRAY_EXECUTABLE_PATH", default="/usr/local/bin/xray")
 XRAY_ASSETS_PATH = config("XRAY_ASSETS_PATH", default="/usr/local/share/xray")
 
-SSL_CERT_FILE = config("SSL_CERT_FILE", default="/var/lib/marzban-node/ssl_cert.pem")
-SSL_KEY_FILE = config("SSL_KEY_FILE", default="/var/lib/marzban-node/ssl_key.pem")
+SSL_CERT_FILE = config(
+    "SSL_CERT_FILE", default="/var/lib/marzban-node/ssl_cert.pem")
+SSL_KEY_FILE = config(
+    "SSL_KEY_FILE", default="/var/lib/marzban-node/ssl_key.pem")
 SSL_CLIENT_CERT_FILE = config("SSL_CLIENT_CERT_FILE", default="")
 
 DEBUG = config("DEBUG", cast=bool, default=False)
 
-SERVICE_PROTOCOL = config('SERVICE_PROTOCOL', cast=str, default='rpyc')
+SERVICE_PROTOCOL = config('SERVICE_PROTOCOL', cast=str, default='rest')
